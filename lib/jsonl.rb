3c000b9b9133c818cc9a259324abfb014065c653
kami-zh <h.zenigami@everyleaf.com>
2017-03-15 23:18:47 +0900

    Implement JSONL.parse

--- lib/jsonl.rb
+++ lib/jsonl.rb
@@ -1,4 +1,14 @@
 require 'jsonl/version'
+require 'json'
 
 module JSONL
+  module_function
+
+  def parse(source, opts = {})
+    parsed = []
+    source.each_line do |line|
+      parsed << JSON.parse(line, opts)
+    end
+    parsed
+  end
 end
