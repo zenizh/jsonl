613af036cea360088ba187b0eb0dea3f809e9569
kami-zh <h.zenigami@everyleaf.com>
2017-03-16 14:17:52 +0900

    Implement `JSONL.generate`

--- lib/jsonl.rb
+++ lib/jsonl.rb
@@ -4,6 +4,18 @@ require 'json'
 module JSONL
   module_function
 
+  def generate(objs, opts = nil)
+    unless objs.is_a?(Array)
+      raise TypeError, "can't generate from #{objs.class}"
+    end
+
+    generated = []
+    objs.map do |obj|
+      generated << JSON.generate(obj, opts)
+    end
+    generated.join("\n")
+  end
+
   def parse(source, opts = {})
     parsed = []
     source.each_line do |line|
--- test/jsonl_test.rb
+++ test/jsonl_test.rb
@@ -5,17 +5,39 @@ class JSONLTest < Minitest::Test
   def setup
     @source = File.read(File.expand_path('../fixtures/source.jsonl', __FILE__))
     @parsed = JSONL.parse(@source)
+    @generated = JSONL.generate(@parsed)
   end
 
-  def test_parsed_class
+  def test_generate_type_error
+    e = assert_raises(TypeError) { JSONL.generate('invalid type') }
+    assert_equal 'can\'t generate from String', e.message
+  end
+
+  def test_generate_generated_type
+    assert_instance_of String, @generated
+  end
+
+  def test_generate_generated_count
+    assert_equal 4, @generated.split("\n").count
+  end
+
+  def test_generate_generated_can_parse
+    assert JSONL.parse(@generated)
+  end
+
+  def test_generate_parsed_equals_to_the_original
+    assert_equal @parsed, JSONL.parse(@generated)
+  end
+
+  def test_parse_parsed_class
     assert_instance_of Array, @parsed
   end
 
-  def test_parsed_count
+  def test_parse_parsed_count
     assert_equal 4, @parsed.count
   end
 
-  def test_parsed_equals_to_json_parse
+  def test_parse_parsed_equals_to_json_parse
     source = @source.split("\n")[0]
     assert_equal JSON.parse(source), @parsed[0]
   end
