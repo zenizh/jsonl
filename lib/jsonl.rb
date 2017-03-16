85624741c27f5f63ae2181d9624423495d8137f2
kami-zh <h.zenigami@everyleaf.com>
2017-03-16 18:10:12 +0900

    Write document comment by RDoc

--- lib/jsonl.rb
+++ lib/jsonl.rb
@@ -4,6 +4,18 @@ require 'json'
 module JSONL
   module_function
 
+  # Generate a string formatted as JSONL from an array.
+  #
+  # ==== Attributes
+  #
+  # * +objs+ - an array consists of objects
+  # * +opts+ - options passes to `JSON.generate`
+  #
+  # ==== Exapmles
+  #
+  #    users = User.all.map(&:attributes) #=> [{"id"=>1, "name"=>"Gilbert", ...}, {"id"=>2, "name"=>"Alexa", ...}, ...]
+  #    generated = JSONL.generate(users)
+  #
   def generate(objs, opts = nil)
     unless objs.is_a?(Array)
       raise TypeError, "can't generate from #{objs.class}"
@@ -16,6 +28,18 @@ module JSONL
     generated.join("\n")
   end
 
+  # Parse JSONL string and return as an array.
+  #
+  # ==== Attributes
+  #
+  # * +source+ - a string formatted as JSONL
+  # * +opts+ - options passes to `JSON.parse`
+  #
+  # ==== Examples
+  #
+  #    source = File.read('source.jsonl')
+  #    parsed = JSONL.parse(source)
+  #
   def parse(source, opts = {})
     parsed = []
     source.each_line do |line|
